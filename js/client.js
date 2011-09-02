fcabd3116902426a86549e03bee84f3bbf13a129
Xm4s <jintetsu@gmail.com>
2011-09-02 16:15:58 +0200

    Switched from DOM Element to canvas: preload assets, update positions on socket message and draw elements in the canvas

--- js/client.js
+++ js/client.js
@@ -3,67 +3,134 @@
 	
 	"use strict";
 	
-	var socket, field, allElem, me, others, update;
+	var Asset, Element, game, socket, all, me;
+	
+	Asset = function Asset(url) {
+		
+		var that = this;
+		
+		this.ready = false;
+		this.img = new Image();
+		this.img.onload = function () { that.ready = true; };
+		this.img.src = url;
+		
+		return this;
+	};
+	
+	Element = function Element(data) {
+		
+		this.id = data.id;
+		this.type = data.type;
+		this.top = data.position.top;
+		this.left = data.position.left;
+		
+		return this;
+	};
+	
+	game = {
+				
+		init: function init() {
+			
+			var canvas, interval;
+			
+			canvas = document.getElementById("canvas");
+			this.ctx = canvas.getContext("2d");
+			
+			this.field = new Asset("gfx/bg.jpg");
+			this.ball = new Asset("gfx/ball.jpg");
+			this.player = new Asset("gfx/player.jpg");
+			this.racket = new Asset("gfx/racket.jpg");
+			
+			interval = setInterval(function () {		
+				if (game.field.ready && game.ball.ready && game.player.ready && game.racket.ready) {
+					clearInterval(interval);
+					socket.init();					
+				}
+			}, 100);
+		},
+		
+		update: function update(data) {
+
+			var id, elem;
+
+			id = data.id;
+			if (all.hasOwnProperty(id)) {
+				elem = all[id];
+				elem.top = data.position.top;
+				elem.left = data.position.left;
+			}
+		},
+		
+		draw: function render(data) {
+			
+			var ball, elem, player;
+			
+			this.ctx.drawImage(this.field.img, 0, 0);	
+			
+			for (elem in all) {
+				if (all.hasOwnProperty(elem)) {
+					elem = all[elem];
+					if (elem.type === 'ball') {
+						this.ctx.drawImage(this.ball.img, elem.left, elem.top + 15);
+					} else if (elem.id !== me.id) {
+						this.ctx.drawImage(this.racket.img, elem.left, elem.top + 15);
+					}
+				}
+			}
+			
+			player = all[me.id];
+			this.ctx.drawImage(this.player.img, player.left, player.top + 15);
+		}
+	};
 		
 	socket = {
 						
 		init: function init() {
 						
 			this.io = io.connect(conf.server, {'port': conf.port});
-		
-			this.io.on('connect', function () {
-				// console.log('Connected to node socket');		
-			});
-		
 			this.io.on('message', function (data) {
 				
-				var id, obj;						
+				var id, obj, elem;						
 				if (data.hasOwnProperty('id')) {
 					
-					id = 'ID-' + data.id;					
-					if (!allElem.hasOwnProperty(id)) {
+					id = data.id;					
+					if (!all.hasOwnProperty(id)) {
 						me.init(data);
 					} else {
-						others.deInit(id);
+						delete all[id];
 					}
 				
 				} else {
 					
 					for (obj in data) {
 						if (data.hasOwnProperty(obj)) {
-							if (!allElem.hasOwnProperty('ID-' + data[obj].id)) {
-								others.init(data[obj]);
+							elem = data[obj];
+							if (!all.hasOwnProperty(elem.id)) {
+								all[elem.id] = new Element(elem);
 							}
-							update(data[obj]);
+							game.update(elem);
 						}
 					}					
 				}
-			});
-		
-			this.io.on('disconnect', function () {
-				// console.log('Disconnected from node socket');
+				
+				game.draw();
 			});
 		}	
 	};
 	
-	field = document.getElementById('field');
-	
-	allElem = {};
-	
+	all = {};
+			
 	me = {
 		
 		init: function init(data) {
-						
-			var id = 'ID-' + data.id;
 			
-			$('.me').remove();
-			$(field).append('<div id="' + id + '" class="racket me"></div>');
-			allElem[id] = document.getElementById(id);
+			this.id = data.id;		
+			this.isMoving = false;
 			
-			update(data);
-						
-			this.isMoving = false;								
-			$(window).bind('keydown', this.onMove).bind('keyup', this.onMove);
+			all[data.id] = new Element(data);
+			
+			window.addEventListener('keydown', this.onMove, true);
+			window.addEventListener('keyup', this.onMove, true);
 		},
 		
 		onMove: function onMove(e) {
@@ -110,32 +177,7 @@
 			}
 		}
 	};
-		
-	others = {
-		
-		init: function init(data) {
-			var id = 'ID-' + data.id;
-			$(field).append('<div id="' + id + '" class="' + data.type + '"></div>');
-			allElem[id] = document.getElementById(id);
-		},
-		
-		deInit: function deInit(id) {
-			$(allElem[id]).remove();
-			delete allElem[id];
-		}
-	};
-		
-	update = function update(data) {
-		
-		var id = 'ID-' + data.id;
-		if (allElem.hasOwnProperty(id)) {
-			$(allElem[id]).css({
-				'top': data.position.top + 'px',
-				'left': data.position.left + 'px'
-			});
-		}
-	};
 	
-	$(document).ready(function () { socket.init(); });
-		
+	$(document).ready(function () { game.init(); });
+	
 }());
